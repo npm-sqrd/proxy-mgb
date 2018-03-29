68c0a27f6d462cce6a9f45d79457a3e04702e359
Mat Bagnall <matbagnall@gmail.com>
2018-03-28 21:37:44 -0700

    created read stream to serve client bundle

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 require('newrelic');
 const http = require('http');
 const serverBundles = require('./getServerBundles');
-const { postData, fetchData, getBundle } = require('./clientHelpers');
+const { postData, fetchData } = require('./clientHelpers');
 const render = require('./renderHtml');
+const fs = require('fs');
+const path = require('path');
 
 // if (typeof window === 'undefined') {
 //   global.window = {};
@@ -30,11 +32,14 @@ const render = require('./renderHtml');
 const app = http.createServer((req, res) => {
   const { method, url } = req;
   const route = url.slice(1).split('/');
+  console.log(url);
   if (method === 'GET') {
     if (route[0] === '') {
       render(res, serverBundles[0]);
     } else if (route[0] === 'res-bundle.js') {
-      getBundle(res, url);
+      const bundleStream = fs.createReadStream(path.join(__dirname, './dist/client-bundles/res-bundle.js'), 'utf8');
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      bundleStream.pipe(res);
     } else if (route[0] === 'restaurants' && route[2] === 'reservations') {
       fetchData(res, url);
     }
